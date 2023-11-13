888a2d92765daf3f562e9c740a6455b763603ef2
Josef Mengele <passionate.dev.313@gmail.com>
2023-11-13 12:36:08 -0500

    update config.py

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,12 +1,13 @@
-from dotenv import dotenv_values
+import os
+from dotenv import load_dotenv
 
 # set config with values in env
-config = dotenv_values(".env")
+load_dotenv()
 
-OPENAI_API_KEY = config["OPENAI_API_KEY"]
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
-PINECONE_KEY = config["PINECONE_KEY"]
-PINECONE_ENV = config["PINECONE_ENV"]
+PINECONE_KEY = os.getenv("PINECONE_KEY")
+PINECONE_ENV = os.getenv("PINECONE_ENV")
 
 INDEX_NAME = "meditermbot-index"
 

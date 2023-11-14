515e202f399fbbde33da22db36957d149190da0a
Josef Mengele <passionate.dev.313@gmail.com>
2023-11-14 03:07:01 -0500

    update frontend

--- frontend/src/pages/manageblog/BlogTable/Modal/CreateBlogModal.js
+++ frontend/src/pages/manageblog/BlogTable/Modal/CreateBlogModal.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import axios from "axios";
+import { Spin } from "antd";
 
 const modules = {
   toolbar: [
@@ -10,6 +11,7 @@ const modules = {
 };
 
 const CreateBlogModal = ({ isOpen, onSubmit, onClose, blog }) => {
+  const [loading, setLoading] = useState(false);
   const [title, setTitle] = useState("");
   const [url, setUrl] = useState("");
   const [content, setContent] = useState("");
@@ -47,8 +49,11 @@ const CreateBlogModal = ({ isOpen, onSubmit, onClose, blog }) => {
   };
 
   const handleSubmit = () => {
+    setLoading(true);
     const sub_content = get_subcontent(content);
-    onSubmit({ title, url, content, sub_content });
+    onSubmit({ title, url, content, sub_content }).then(() => {
+      setLoading(false);
+    });
     removeContent();
   };
 
@@ -74,55 +79,61 @@ const CreateBlogModal = ({ isOpen, onSubmit, onClose, blog }) => {
 
   return (
     <div className="fixed z-10 inset-0 overflow-y-auto flex items-center justify-center bg-gray-900/30 p-2">
-      <div className="bg-white rounded-lg shadow-md h-full md:h-3/4 w-full md:w-1/2 m-2 flex flex-col">
-        <div className="bg-indigo-700 px-6 py-4 text-white rounded-t-lg flex justify-between items-center">
-          <h3 className="text-lg leading-6 font-medium">Blog</h3>
-          <button onClick={handleClose} className="text-white">
-            X
-          </button>
+      {loading ? (
+        <div className="fixed z-50 w-full h-full bg-black bg-opacity-50 flex items-center justify-center">
+          <Spin />
         </div>
-        <div className="px-6 py-4 flex-grow overflow-auto">
-          <div className="flex flex-col h-full">
-            <input
-              className="border mb-4 p-2 w-full rounded-lg"
-              type="text"
-              placeholder="Title"
-              value={title}
-              onChange={(e) => changedTitle(e.target.value)}
-            />
-            <input
-              className="border mb-4 p-2 w-full rounded-lg"
-              type="text"
-              placeholder="URL"
-              value={url}
-              onChange={(e) => setUrl(e.target.value)}
-            />
-            <div className="flex-grow pb-16">
-              <ReactQuill
-                theme="snow"
-                modules={modules}
-                value={content}
-                onChange={setContent}
-                className="h-full text-gray-900"
+      ) : (
+        <div className="bg-white rounded-lg shadow-md h-full md:h-3/4 w-full md:w-1/2 m-2 flex flex-col">
+          <div className="bg-indigo-700 px-6 py-4 text-white rounded-t-lg flex justify-between items-center">
+            <h3 className="text-lg leading-6 font-medium">Blog</h3>
+            <button onClick={handleClose} className="text-white">
+              X
+            </button>
+          </div>
+          <div className="px-6 py-4 flex-grow overflow-auto">
+            <div className="flex flex-col h-full">
+              <input
+                className="border mb-4 p-2 w-full rounded-lg"
+                type="text"
+                placeholder="Title"
+                value={title}
+                onChange={(e) => changedTitle(e.target.value)}
               />
-            </div>
-            <div className="flex justify-end">
-              <button
-                onClick={handleClose}
-                className="bg-white text-gray-500 border border-gray-300 hover:border-gray-500 p-2 mt-4 rounded-lg transition-colors duration-150 hover:text-black w-20"
-              >
-                Close
-              </button>
-              <button
-                className="bg-indigo-700 text-white p-2 mt-4 rounded-lg transition-colors duration-150 hover:bg-indigo-800 ml-2 w-20"
-                onClick={handleSubmit}
-              >
-                Submit
-              </button>
+              <input
+                className="border mb-4 p-2 w-full rounded-lg"
+                type="text"
+                placeholder="URL"
+                value={url}
+                onChange={(e) => setUrl(e.target.value)}
+              />
+              <div className="flex-grow pb-16">
+                <ReactQuill
+                  theme="snow"
+                  modules={modules}
+                  value={content}
+                  onChange={setContent}
+                  className="h-full text-gray-900"
+                />
+              </div>
+              <div className="flex justify-end">
+                <button
+                  onClick={handleClose}
+                  className="bg-white text-gray-500 border border-gray-300 hover:border-gray-500 p-2 mt-4 rounded-lg transition-colors duration-150 hover:text-black w-20"
+                >
+                  Close
+                </button>
+                <button
+                  className="bg-indigo-700 text-white p-2 mt-4 rounded-lg transition-colors duration-150 hover:bg-indigo-800 ml-2 w-20"
+                  onClick={handleSubmit}
+                >
+                  Submit
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
